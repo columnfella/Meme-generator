f52bf9f4b476f66552988b8c53e67bc50242272a
Saadeddine El Hajjaji <sa.elhajjaji@aui.ma>
2025-07-19 12:09:46 +0000

    adjusted README

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# React + Vite
+# Meme Generator
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This repository is a small React-built web app that uses the Imgflip API to generate random meme images where you can modify the top and bottom text.
 
-Currently, two official plugins are available:
+###### *Recources:*
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+This repository is an exercise from Bob Zirroll's React course on the Scrimba platform: [https://scrimba.com/learn-react-c0e](https://scrimba.com/learn-react-c0e)
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+The Imgflip API: https://imgflip.com/api
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,8 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 export default function Main() {
     const [meme, setMeme] = useState({
@@ -6,44 +10,53 @@ export default function Main() {
         bottomText: "Walk into Mordor",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
     })
+    const [memeImgs, setMemeImgs] = useState()
     
+    useEffect(()=>(
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res=>res.json())
+            .then(data=>setMemeImgs(data.data.memes))
+    ), [])
+
+    function handleGenerateNewMemeImage() {
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            imageUrl: memeImgs[getRandomInt(0, memeImgs.length)].url
+        }))
+    }
+
     function handleOnChange(event) {
         const {value, name} = event.currentTarget;
         console.log(value);
-        if (name === "bottomText") {
-            setMeme(prevMeme => ({
-                ...prevMeme,
-                bottomText: value
-            }))
-            return;
-        }
         setMeme(prevMeme => ({
             ...prevMeme,
-            topText: value
+            [name]: value
         }))
     }
 
     return (
         <main>
             <div className="form">
-                <label>Top Text
+                <label>Top Text:
                     <input
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
                         onChange={handleOnChange}
+                        value={meme.topText}
                     />
                 </label>
 
-                <label>Bottom Text
+                <label>Bottom Text:
                     <input
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
                         onChange={handleOnChange}
+                        value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={handleGenerateNewMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.imageUrl} />
